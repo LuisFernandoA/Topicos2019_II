8652948f8f18a4e4f1d0b2bdd07aa5910b3fcff9
LuisFernandoA <58788386+LuisFernandoA@users.noreply.github.com>
2019-12-11 16:43:41 -0500

    Update PROYECTO1.java

--- src/proyecto1/PROYECTO1.java
+++ src/proyecto1/PROYECTO1.java
@@ -5,7 +5,7 @@ public class PROYECTO1 {
 
     public static void main(String[] args) {
         System.out.println("Primera Modificacion");
-        System.out.println("Primera Modificacion");
+        System.out.println("segunda Modificacion");
     }
     
 }
